f475493d54bea7f55fc4727f9c712b60d150f3a3
Walyson Moura <walysonmoura222@gmail.com>
2022-09-24 14:26:57 -0300

    new

--- src/components/social/style.js
+++ src/components/social/style.js
@@ -1,23 +1,31 @@
 import styled from "styled-components";
+import { Colors } from "../../styles/colors";
 
 export const List = styled.ul`
-position: fixed;
-z-index: 3;
-bottom: 0;
-left: 1rem;
+  position: fixed;
+  z-index: 3;
+  bottom: 0;
+  left: 1rem;
 
-display: flex;
-flex-direction: column;
-align-items: center;
-gap: 1rem;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  gap: 1.5rem;
 
-::after{
+  color: ${Colors.branco};
+  li {
+    :hover {
+      transform: translateY(-0.25rem);
+      transition: 0.3s;
+      color: ${Colors.azul1};
+    }
+  }
+  ::after {
     content: "";
     display: block;
     width: 1px;
-    height: 90px;
+    height: 100px;
     margin: 0px auto;
     background-color: #a8b2d1;
-
-}
-`
+  }
+`;
